54eb5ca21df5750c4d3ab322b8a9bd04619e0dab
dave <toy356@gmail.com>
2026-09-11 16:44:23 +1200

    coding-sep-11

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-alert('Hello World');
+let a = 6;
+let b = 2;
+
+let myName = "Ryu";
+
+console.log(a+b);
+console.log(a*b);
+console.log(a/b);
+
+console.log("Hello " + myName);
+
+myName = "John";
+
+console.log("New name is " + myName)
