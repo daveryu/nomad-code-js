d81b4114333b23bb81f0ecd1f31ceba13cc7de8b
dave <toy356@gmail.com>
2026-09-21 18:01:09 +1200

    coding-sep-21

--- app.js
+++ app.js
@@ -1,35 +1,7 @@
-const calculator = {
-    add: function (a, b) {
-        console.log(a + b);
-    },
-    minus: function (a, b) {
-        console.log(a - b);
-    },
-    divide: function (a, b) {
-        console.log(a / b);
-    },
-    times: function (a, b) {
-        console.log(a*b);
-    },
-    power: function (a, b) {
-        console.log(a**b);
-    },
-};
+let age = parseInt(prompt("How ald are you?"));
 
-
-calculator.add(1,2);
-calculator.minus(1,2);
-calculator.divide(1,2);
-calculator.times(3,2);
-calculator.power(2,3);
-
-
-
-const age = 96;
-function calculateKrAge(ageOfForeigner) {
-    return ageOfForeigner +2;
-};
-
-const krAge = calculateKrAge(age);
-
-console.log(krAge);
+while(isNaN(age)){
+    // condition === true
+    age = parseInt(prompt("Please write a number"));
+}
+console.log("Thank you for writing your age.");
